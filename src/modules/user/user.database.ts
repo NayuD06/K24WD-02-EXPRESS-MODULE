5716443ad5192f6f70429012e58c74a4e3db6983
Duy An <anclashofclanshall13@gmail.com>
2026-03-05 12:47:43 +0700

    update user

--- src/modules/user/user.database.ts
+++ src/modules/user/user.database.ts
@@ -22,6 +22,20 @@ export class UserDatabase {
     const res = await this.col().insertOne(doc);
     return { _id: res.insertedId, ...doc };
   }
-  // BTVN: insertMany, UpdateOne, DeleteOne
+
+  async insertMany(docs: UserDoc[]): Promise<Array<UserEntity>> {
+    const res = await this.col().insertMany(docs);
+    return docs.map((doc, i) => ({ _id: res.insertedIds[i] as ObjectId, ...doc }));
+  }
+
+  async updateOne(id: string, update: Partial<UserDoc>): Promise<UserEntity | null> {
+    await this.col().updateOne({ _id: new ObjectId(id) }, { $set: update });
+    return this.findById(id);
+  }
+
+  async deleteOne(id: string): Promise<boolean> {
+    const res = await this.col().deleteOne({ _id: new ObjectId(id) });
+    return res.deletedCount === 1;
+  }
 }
 
